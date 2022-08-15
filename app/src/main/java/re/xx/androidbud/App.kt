3fc41170076af4dc2524266460da1a0fed13f94f
Jiaxin Peng <pjx206@163.com>
2022-08-16 02:03:23 +0800

    Automatically detect ABI to set default frida server

--- app/src/main/java/re/xx/androidbud/App.kt
+++ app/src/main/java/re/xx/androidbud/App.kt
@@ -2,13 +2,54 @@ package re.xx.androidbud
 
 import android.app.Application
 import android.content.IntentFilter
+import android.os.Build
+import android.util.Log
+import androidx.preference.PreferenceManager
+import java.util.*
 
 class App : Application() {
+    private val TAG = "App"
+    private val keyFirstStart = "isFirstStart"
+
     override fun onCreate() {
         super.onCreate()
+
+        initializeApp()
         setupClipboardReceiver()
     }
 
+    private fun initializeApp() {
+        val sPrefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val isFirstStart = sPrefs.getBoolean(keyFirstStart, true)
+
+        if (isFirstStart) {
+            val fridaServerName: String = when (Build.SUPPORTED_ABIS[0]) {
+                "arm64-v8a" -> {
+                    "frida-server-15.2.2-android-arm64"
+                }
+                "armeabi-v7a" -> {
+                    "frida-server-15.2.2-android-arm"
+                }
+                "armeabi" -> {
+                    "frida-server-15.2.2-android-arm"
+                }
+                "x86_64" -> {
+                    "frida-server-15.2.2-android-x86_64"
+                }
+                "x86" -> {
+                    "frida-server-15.2.2-android-x86"
+                }
+                else -> {
+                    ""
+                }
+            }
+            val editor = sPrefs.edit()
+            Log.d(TAG, "initializeApp: frida server:  $fridaServerName")
+            editor.putBoolean(keyFirstStart, false)
+                .putString("frida_server_name", fridaServerName)
+                .commit()
+        }
+    }
 
     private fun setupClipboardReceiver() {
         val cbReceiver = ClipboardReceiver()
