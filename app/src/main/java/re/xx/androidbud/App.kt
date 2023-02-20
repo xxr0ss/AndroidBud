94d70be677e56e598564a7391f4f549c5ef542a8
Jiaxin Peng <pjx3231@163.com>
2023-02-21 00:23:37 +0800

    Update

--- app/src/main/java/re/xx/androidbud/App.kt
+++ app/src/main/java/re/xx/androidbud/App.kt
@@ -21,22 +21,23 @@ class App : Application() {
         val sPrefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
         val isFirstStart = sPrefs.getBoolean(keyFirstStart, true)
 
+        val fridaVersion = "16.0.10"
         if (isFirstStart) {
             val fridaServerName: String = when (Build.SUPPORTED_ABIS[0]) {
                 "arm64-v8a" -> {
-                    "frida-server-15.2.2-android-arm64"
+                    "frida-server-$fridaVersion-android-arm64"
                 }
                 "armeabi-v7a" -> {
-                    "frida-server-15.2.2-android-arm"
+                    "frida-server-$fridaVersion-android-arm"
                 }
                 "armeabi" -> {
-                    "frida-server-15.2.2-android-arm"
+                    "frida-server-$fridaVersion-android-arm"
                 }
                 "x86_64" -> {
-                    "frida-server-15.2.2-android-x86_64"
+                    "frida-server-$fridaVersion-android-x86_64"
                 }
                 "x86" -> {
-                    "frida-server-15.2.2-android-x86"
+                    "frida-server-$fridaVersion-android-x86"
                 }
                 else -> {
                     ""
@@ -46,7 +47,7 @@ class App : Application() {
             Log.d(TAG, "initializeApp: frida server:  $fridaServerName")
             editor.putBoolean(keyFirstStart, false)
                 .putString("frida_server_name", fridaServerName)
-                .commit()
+                .apply()
         }
     }
 
